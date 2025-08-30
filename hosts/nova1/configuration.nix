af74bfda82ef370f0996331bdd1c6233b45207b9
zimbatm <zimbatm@zimbatm.com>
2025-08-30 18:25:28 +0200

    enable flakes

--- hosts/nova1/configuration.nix
+++ hosts/nova1/configuration.nix
@@ -98,6 +98,9 @@
     vim
   ];
 
+  # Enable Flakes and the new Nix CLI
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Some programs need SUID wrappers, can be configured further or are started in user sessions. programs.mtr.enable = true;
   # programs.gnupg.agent = {
   #   enable = true; enableSSHSupport = true;
